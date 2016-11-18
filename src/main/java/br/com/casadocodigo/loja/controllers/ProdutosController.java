c29ece76cc93caaff0a75152e16686cf64641e4a
Fernando Nakagawa <fernandonkgw@gmail.com>
2016-11-18 10:40:40 -0200

    Validacao do Produto ao ser cadastrado

--- src/main/java/br/com/casadocodigo/loja/controllers/ProdutosController.java
+++ src/main/java/br/com/casadocodigo/loja/controllers/ProdutosController.java
@@ -2,9 +2,14 @@ package br.com.casadocodigo.loja.controllers;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -12,6 +17,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import br.com.casadocodigo.loja.daos.ProdutoDao;
 import br.com.casadocodigo.loja.models.Produto;
 import br.com.casadocodigo.loja.models.TipoPreco;
+import br.com.casadocodigo.loja.validation.ProdutoValidation;
 
 @Controller
 @RequestMapping("produtos")
@@ -20,6 +26,11 @@ public class ProdutosController {
 	@Autowired
 	private ProdutoDao produtoDao;
 
+	@InitBinder
+	public void initBinder(WebDataBinder binder) {
+		binder.addValidators(new ProdutoValidation());
+	}
+	
 	@RequestMapping("form")
 	public String form(Model model) {
 		
@@ -29,9 +40,12 @@ public class ProdutosController {
 	}
 
 	@RequestMapping(method = RequestMethod.POST)
-	public String gravar(Produto produto, RedirectAttributes redirectAttributes) {
+	public String gravar(@Valid Produto produto, BindingResult result, RedirectAttributes redirectAttributes, Model model) {
+		
+		if (result.hasErrors()) {
+			return form(model);
+		}
 
-		System.out.println(produto);
 		produtoDao.gravar(produto);
 		
 		redirectAttributes.addFlashAttribute("sucesso","Produto cadastrado com sucesso!");
