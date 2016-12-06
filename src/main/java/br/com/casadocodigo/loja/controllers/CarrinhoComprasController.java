466c5a28067a85bd2493d8c2fbabf539aa7cc539
Fernando Nakagawa <fernandonkgw@gmail.com>
2016-12-06 17:25:09 -0200

    método para criar um CarrinhoItem

--- src/main/java/br/com/casadocodigo/loja/controllers/CarrinhoComprasController.java
+++ src/main/java/br/com/casadocodigo/loja/controllers/CarrinhoComprasController.java
@@ -1,17 +1,34 @@
 package br.com.casadocodigo.loja.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import br.com.casadocodigo.loja.daos.ProdutoDao;
+import br.com.casadocodigo.loja.models.CarrinhoItem;
+import br.com.casadocodigo.loja.models.Produto;
 import br.com.casadocodigo.loja.models.TipoPreco;
 
 @Controller
 @RequestMapping("/carrinho")
 public class CarrinhoComprasController {
 
+	@Autowired
+	private ProdutoDao produtoDao;
+	
 	@RequestMapping("add")
 	public String add(Integer produtoId, TipoPreco tipo) {
 		
+		CarrinhoItem carrinhoItem = criaCarrinhoItem(produtoId, tipo);
+		
 		return "redirect:/produtos";
+	}
+	
+	private CarrinhoItem criaCarrinhoItem(Integer produtoId, TipoPreco tipo) {
+		
+		Produto produto = produtoDao.find(produtoId);
+		CarrinhoItem carrinhoItem = new CarrinhoItem(tipo, produto);
+		
+		return carrinhoItem;
 	}
 }
