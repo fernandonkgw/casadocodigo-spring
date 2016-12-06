05837f12d4d3926cf39403be32e6548cc87b0ab7
Fernando Nakagawa <fernandonkgw@gmail.com>
2016-12-06 17:33:20 -0200

    Adicionado o pacote dos models para o spring scanear

--- src/main/java/br/com/casadocodigo/loja/conf/AppWebConfiguration.java
+++ src/main/java/br/com/casadocodigo/loja/conf/AppWebConfiguration.java
@@ -18,9 +18,11 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import br.com.casadocodigo.loja.controllers.HomeController;
 import br.com.casadocodigo.loja.daos.ProdutoDao;
 import br.com.casadocodigo.loja.infra.FileSaver;
+import br.com.casadocodigo.loja.models.CarrinhoCompras;
 
 @EnableWebMvc
-@ComponentScan(basePackageClasses = { HomeController.class, ProdutoDao.class, FileSaver.class })
+@ComponentScan(basePackageClasses = { HomeController.class, ProdutoDao.class, FileSaver.class,
+		CarrinhoCompras.class})
 public class AppWebConfiguration extends WebMvcConfigurerAdapter {
 
 	@Bean
