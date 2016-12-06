a3ccf08d4870e61f3610fe94f55f309103de35f5
Fernando Nakagawa <fernandonkgw@gmail.com>
2016-12-06 17:35:26 -0200

    criado instancia do carrinho no controller para adicionar o item

--- src/main/java/br/com/casadocodigo/loja/controllers/CarrinhoComprasController.java
+++ src/main/java/br/com/casadocodigo/loja/controllers/CarrinhoComprasController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import br.com.casadocodigo.loja.daos.ProdutoDao;
+import br.com.casadocodigo.loja.models.CarrinhoCompras;
 import br.com.casadocodigo.loja.models.CarrinhoItem;
 import br.com.casadocodigo.loja.models.Produto;
 import br.com.casadocodigo.loja.models.TipoPreco;
@@ -16,11 +17,14 @@ public class CarrinhoComprasController {
 	@Autowired
 	private ProdutoDao produtoDao;
 	
+	@Autowired
+	private CarrinhoCompras carrinho;
+	
 	@RequestMapping("add")
 	public String add(Integer produtoId, TipoPreco tipo) {
 		
 		CarrinhoItem carrinhoItem = criaCarrinhoItem(produtoId, tipo);
-		
+		carrinho.add(carrinhoItem);
 		return "redirect:/produtos";
 	}
 	
