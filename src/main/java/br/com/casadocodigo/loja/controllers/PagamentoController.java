01d86c1855a36e50439e1ac649fb82eea4e92942
Fernando Nakagawa <fernandonkgw@gmail.com>
2016-12-07 15:05:08 -0200

    integrando rest de pagamentos

--- src/main/java/br/com/casadocodigo/loja/controllers/PagamentoController.java
+++ src/main/java/br/com/casadocodigo/loja/controllers/PagamentoController.java
@@ -1,15 +1,21 @@
 package br.com.casadocodigo.loja.controllers;
 
+import java.awt.datatransfer.Clipboard;
+import java.util.concurrent.Callable;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import br.com.casadocodigo.loja.models.CarrinhoCompras;
+import br.com.casadocodigo.loja.models.DadosPagamento;
 
 @Controller
 @RequestMapping("/pagamento")
@@ -19,11 +25,25 @@ public class PagamentoController {
 	@Autowired
 	private CarrinhoCompras carrinho;
 	
+	@Autowired
+	private RestTemplate restTemplate;
+	
 	@RequestMapping(value="finalizar", method=RequestMethod.POST)
-	public ModelAndView finalizar(RedirectAttributes model) {
-		System.out.println(carrinho.getTotal());
-		model.addFlashAttribute("sucesso", "Pagamento realizado com sucesso!");
-		return new ModelAndView("redirect:/produtos");
+	public Callable<ModelAndView> finalizar(RedirectAttributes model) {
+		return () -> {
+			try {
+				String url = "http://book-payment.herokuapp.com/payment";
+				String response = restTemplate.postForObject(url, new DadosPagamento(carrinho.getTotal()), String.class);
+				
+				model.addFlashAttribute("mensagem", response);
+				System.out.println(response);
+				return new ModelAndView("redirect:/produtos");
+			} catch (HttpClientErrorException e) {
+				
+				model.addFlashAttribute("falha", "Valor maior que permitido");
+				return new ModelAndView("redirect:/produtos");
+			}
+		};
 	}
 	
 }
