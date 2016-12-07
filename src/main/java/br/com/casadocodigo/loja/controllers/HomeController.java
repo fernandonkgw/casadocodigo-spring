771162e39cb979e946e1ee618513901d1ddcbe9a
Fernando Nakagawa <fernandonkgw@gmail.com>
2016-12-07 16:30:03 -0200

    refatoração da classe pois ao passar o Model como parâmetro o cache não estava funcionando

--- src/main/java/br/com/casadocodigo/loja/controllers/HomeController.java
+++ src/main/java/br/com/casadocodigo/loja/controllers/HomeController.java
@@ -3,24 +3,27 @@ package br.com.casadocodigo.loja.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import br.com.casadocodigo.loja.daos.ProdutoDao;
 import br.com.casadocodigo.loja.models.Produto;
 
 @Controller
+@RequestMapping("/")
 public class HomeController {
 
 	@Autowired
 	private ProdutoDao produtoDao;
 	
 	@RequestMapping("/")
-	public String index(Model model) {
+	@Cacheable(value="produtosHome")
+	public ModelAndView index() {
 		List<Produto> produtos = produtoDao.listar();
-		model.addAttribute("produtos", produtos);
-		System.out.println("Entrando na Home da CDC");
-		return "home";
+		ModelAndView model = new ModelAndView("home");
+		model.addObject("produtos", produtos);
+		return model;
 	}
 }
