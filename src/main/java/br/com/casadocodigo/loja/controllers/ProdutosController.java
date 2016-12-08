317a9d5f259ae380cfa931214bd0f33b5762dcda
Fernando Nakagawa <fernandonkgw@gmail.com>
2016-12-08 12:04:17 -0200

    retornando um produto como json

--- src/main/java/br/com/casadocodigo/loja/controllers/ProdutosController.java
+++ src/main/java/br/com/casadocodigo/loja/controllers/ProdutosController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -79,5 +80,11 @@ public class ProdutosController {
 		model.addAttribute("produto", produto);
 		
 		return "produtos/detalhe";
+	}
+	
+	@RequestMapping("{id}")
+	@ResponseBody
+	public Produto detalheJSON(@PathVariable("id") Integer id) {
+		return produtoDao.find(id);
 	}
 }
