b9158bc6648a9ef48d07ae65f3125ed1127575b7
Fernando Nakagawa <fernandonkgw@gmail.com>
2016-12-15 10:18:13 -0200

    método para calcular a soma dos preços do produto por tipo

--- src/main/java/br/com/casadocodigo/loja/daos/ProdutoDao.java
+++ src/main/java/br/com/casadocodigo/loja/daos/ProdutoDao.java
@@ -1,14 +1,17 @@
 package br.com.casadocodigo.loja.daos;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import br.com.casadocodigo.loja.models.Produto;
+import br.com.casadocodigo.loja.models.TipoPreco;
 
 @Repository
 @Transactional
@@ -34,4 +37,9 @@ public class ProdutoDao {
 				.getSingleResult();
 	}
 	
+	public BigDecimal somaPrecosPorTipo(TipoPreco tipoPreco) {
+		TypedQuery<BigDecimal> query = manager.createQuery("select sum(preco.valor) from Produto p join p.precos preco where preco.tipo = :tipoPreco", BigDecimal.class)
+				.setParameter("tipoPreco", tipoPreco);
+		return query.getSingleResult();
+	}
 }
